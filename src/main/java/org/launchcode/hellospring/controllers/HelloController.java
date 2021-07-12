859dc7291edf654e020d51724d9f5998459cf901
RyanCEaton <ryan_eaton@comcast.com>
2021-07-12 08:29:25 -0600

    Chapter 10 exercise done

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -17,24 +17,42 @@ public class HelloController {
 //    }
 
 
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
+//    // lives at /hello/goodbye
+//    @GetMapping("goodbye")
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//    }
+//
+//    // Create a handler request of the form /hello?name=LaunchCode
+
+    @RequestMapping(method = {RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+      return message(name, language);
     }
+    public static String message(String name, String language) {
 
-    // Create a handler request of the form /hello?name=LaunchCode
-
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+        if (language.equals("english")) {
+            return "Hello, " + name + "!";
+        }
+        else if (language.equals("french")) {
+            return "Bonjour, " + name + "!";
+        }
+        else if (language.equals("spanish")) {
+            return "Hola, " + name + "!";
+        }
+        else if (language.equals("german")) {
+            return "Hallo, " + name + "!";
+        }
+        else if (language.equals("japanese")) {
+            return "Kon'nichiwa, " + name + "!";
+        }
+        return "Hello, world!";
     }
-
     // Handles requests of the for /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
+//    @GetMapping("{name}")
+//    public String helloWithPathParam(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     // lives at /hello/form
     @GetMapping("form")
@@ -42,7 +60,14 @@ public class HelloController {
         return "<html>" +
                 "<body>" +
                 "<form action = '/hello' method = 'post'>" + //submit a request to /hello
-                "<input type = 'text' name = 'name'>" +
+                "<input type = 'text' id = 'name' name = 'name'>" +
+                "<select id = 'language' name = 'language'>" +
+                "<option value = 'english'>English</option>" +
+                "<option value = 'french'>French</option>" +
+                "<option value = 'spanish'>Spanish</option>" +
+                "<option value = 'german'>German</option>" +
+                "<option value = 'japanese'>Japanese</option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet me!'>" +
                 "</form>" +
                 "</body>" +
